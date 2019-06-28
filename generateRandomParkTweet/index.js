00e84537950c1a8fa6ea5a12bac1c4c96eb9d15a
Raymond Camden <raymondcamden@gmail.com>
2019-06-28 09:39:01 -0500

    more damn debugging5

--- generateRandomParkTweet/index.js
+++ generateRandomParkTweet/index.js
@@ -78,12 +78,6 @@ function getRandomIntInclusive(min, max) {
 module.exports = async function (context, req) {
     context.log('checking my env', process.env.TWITTER_ACCESS_TOKEN, ' and ', process.env.TWITTER_ACCESS_TOKEN_SECRET);
 
-    // temporary test
-    context.res = {
-        body: `process env test ${process.env.TWITTER_ACCESS_TOKEN} and ${process.env.TWITTER_ACCESS_TOKEN_SECRET} now my consumer key ${process.env.TWITTER_CONSUMER_KEY} and secret ${process.env.TWITTER_CONSUMER_SECRET}`
-    };
-    context.done();
-
     const T = new Twit({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
